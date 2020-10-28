d8966e86361dbea295a610d64ae69c6dfee779c9
Marcospaulo-paixao <contato.marcospaulo1@gmail.com>
2020-10-28 00:02:31 -0300

    AppVisualizar e doc modelo logico

--- TorrentzFilmes/src/br/com/torrentz/app/AppVisualizar.java
+++ TorrentzFilmes/src/br/com/torrentz/app/AppVisualizar.java
@@ -1,3 +1,5 @@
+package br.com.torrentz.app;
+
 
 import java.awt.Color;
 import java.awt.event.ActionEvent;
@@ -16,11 +18,11 @@ import javax.swing.Timer;
  *
  * @author marcos
  */
-public class AppVisualizacao extends javax.swing.JFrame {
+public class AppVisualizar extends javax.swing.JFrame {
     /**
      * Creates new form AppVisualizacao
      */
-    public AppVisualizacao() {
+    public AppVisualizar() {
         initComponents();
     }
 
@@ -41,33 +43,33 @@ public class AppVisualizacao extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButtonPlay.setIcon(new javax.swing.ImageIcon("/mnt/HD-500/projetos/ads20192-modulo3/gp/Torrentz/marcos/ProjetoTorrentz/assets/botao-play.png")); // NOI18N
+        jButtonPlay.setIcon(new javax.swing.ImageIcon("/home/marcos/development/NetBeans/NetbeansProject/ProjetoTorrentz/assets/botao-play.png")); // NOI18N
         jButtonPlay.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButtonPlayActionPerformed(evt);
             }
         });
 
-        jLabel1.setIcon(new javax.swing.ImageIcon("/mnt/HD-500/projetos/ads20192-modulo3/gp/Torrentz/marcos/ProjetoTorrentz/assets/Pantera-Negra.jpg")); // NOI18N
+        jLabel1.setIcon(new javax.swing.ImageIcon("/home/marcos/development/NetBeans/NetbeansProject/ProjetoTorrentz/assets/Pantera-Negra.jpg")); // NOI18N
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(20, 20, 20)
                 .addComponent(jLabel1)
-                .addContainerGap())
+                .addGap(20, 20, 20))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addContainerGap()
+                .addGap(20, 20, 20)
                 .addComponent(jLabel1)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(20, Short.MAX_VALUE))
         );
 
-        jButtonPause.setIcon(new javax.swing.ImageIcon("/mnt/HD-500/projetos/ads20192-modulo3/gp/Torrentz/marcos/ProjetoTorrentz/assets/simbolo-de-pausa.png")); // NOI18N
+        jButtonPause.setIcon(new javax.swing.ImageIcon("/home/marcos/development/NetBeans/NetbeansProject/ProjetoTorrentz/assets/simbolo-de-pausa.png")); // NOI18N
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -76,12 +78,12 @@ public class AppVisualizacao extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+            .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jButtonPause)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jButtonPlay)
-                .addContainerGap())
+                .addGap(20, 20, 20))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -89,9 +91,9 @@ public class AppVisualizacao extends javax.swing.JFrame {
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButtonPause, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jButtonPlay, javax.swing.GroupLayout.DEFAULT_SIZE, 62, Short.MAX_VALUE))
-                .addContainerGap())
+                    .addComponent(jButtonPause)
+                    .addComponent(jButtonPlay))
+                .addContainerGap(20, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -133,20 +135,21 @@ public class AppVisualizacao extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(AppVisualizacao.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AppVisualizar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(AppVisualizacao.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AppVisualizar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(AppVisualizacao.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AppVisualizar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(AppVisualizacao.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AppVisualizar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new AppVisualizacao().setVisible(true);
+                new AppVisualizar().setVisible(true);
             }
         });
     }
